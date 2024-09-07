76c08ae40dba4279030d15abf118a65537e77294
douxiaobo <dxb_1020@icloud.com>
2024-09-07 18:54:53 +0800

    use i18n

---
+++ Personal_Website_Go_gogf_i18n/go.mod
@@ -0,0 +1,22 @@
+module modole
+
+go 1.22.3
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/clbanning/mxj/v2 v2.7.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogf/gf/v2 v2.7.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- Personal_Website_Go_gogf_i18n/main.go
+++ Personal_Website_Go_gogf_i18n/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/gogf/gf/v2/i18n/gi18n"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 type User struct {
@@ -13,11 +16,21 @@ type User struct {
 	title    string
 }
 
-var languages_ranges = [...]string{"en", "zh", "es"}
+var languages_ranges = [...]string{"en", "zh-CN", "es"}
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	var user User
 	user.language = getLanguageFromHeader(r.Header.Get("Accept-Language"))
+	var (
+		ctx  = gctx.New()
+		i18n = gi18n.New()
+	)
+
+	i18n.SetLanguage(user.language)
+	user.title = i18n.Translate(ctx, "title")
+	user.name = i18n.Translate(ctx, "name")
+	fmt.Fprintf(w, user.title)    //Welcome to my website!
+	fmt.Fprintf(w, user.name)     //Douxiaobao
 	fmt.Fprintf(w, user.language) //en,zh-CN;q=0.9,zh;q=0.8,es;q=0.7
 
 }
@@ -32,7 +45,7 @@ func getLanguageFromHeader(header string) string {
 			}
 		}
 	}
-	return ""
+	return "en"
 }
 func main() {
 	defer func() {
