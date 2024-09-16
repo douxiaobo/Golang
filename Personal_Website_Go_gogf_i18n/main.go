bdb881a24c98fa8475f8e563afd1f510a733175e
douxiaobo <dxb_1020@icloud.com>
2024-09-16 16:36:55 +0800

    20240916

--- Personal_Website_Go_gogf_i18n/main.go
+++ Personal_Website_Go_gogf_i18n/main.go
@@ -12,16 +12,90 @@ import (
 )
 
 type User struct {
-	Language string
-	Name     string
-	Title    string
+	Language    string
+	Name        string
+	Title       string
+	ContentName string
+	FooterLinks []FooterLink
 }
 
-var languages_ranges = [...]string{"en", "zh-CN", "es"}
+type FooterLink struct {
+	Lang                  string
+	Footer_language_short string
+	Footer_language_long  string
+}
+
+var languages_ranges = [...]string{"en", "zh", "es"}
+
+var containtlist = [...]string{"home", "about", "work", "travel", "music", "programming", "school", "sport"}
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	var user User
-	user.Language = getLanguageFromHeader(r.Header.Get("Accept-Language"))
+	// 从URL路径中获取语言后缀
+	path := strings.TrimPrefix(r.URL.Path, "/")
+	pathParts := strings.Split(path, "/")
+
+	// // 如果路径为空或仅为 "/"
+	// if len(pathParts) == 0 || (len(pathParts) == 1 && pathParts[0] == "") {
+	// 	// 设置默认语言和内容名称
+	// 	user.Language = getLanguageFromHeader(r.Header.Get("Accept-Language"))
+	// 	user.ContentName = "home"
+	// }
+
+	// // 如果路径中有足够的部分，那么第一部分是语言，第二部分是内容名称
+	// if len(pathParts) >= 2 {
+	// 	user.Language = pathParts[0]
+	// 	user.ContentName = pathParts[1]
+
+	// 	if !(isValidLang(pathParts[0]) || isValidContentName(pathParts[1])) {
+	// 		http.Error(w, "Invaid URL", http.StatusBadGateway)
+	// 		return
+	// 	}
+	// } else if len(pathParts) >= 2 && pathParts[1] == "" {
+	// 	user.Language = pathParts[0]
+	// 	user.ContentName = "home"
+	// } else if len(pathParts) == 1 {
+	// 	user.Language = pathParts[0]
+	// 	// 如果没有指定内容名称，默认使用home
+	// 	if !isValidLang(user.Language) {
+	// 		http.Error(w, "Invalid URL", http.StatusBadRequest)
+	// 		return
+	// 	}
+	// 	// user.ContentName = "home"
+	// } else {
+	// 	user.Language = getLanguageFromHeader(r.Header.Get("Accept-Language"))
+	// 	// user.ContentName = "home" // 这里替换为你的默认内容名称
+	// }
+
+	// 如果路径为空或仅为 "/"
+	if len(pathParts) == 0 || (len(pathParts) == 1 && pathParts[0] == "") {
+		// 设置默认语言和内容名称
+		user.Language = getLanguageFromHeader(r.Header.Get("Accept-Language"))
+		user.ContentName = "home"
+	} else if len(pathParts) == 1 || len(pathParts) == 2 && pathParts[1] == "" {
+		user.Language = pathParts[0]
+		user.ContentName = "home"
+	} else if len(pathParts) == 2 {
+		user.Language = pathParts[0]
+		user.ContentName = pathParts[1]
+	} else {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	// 检查语言是否在支持的范围内
+	if !isValidLang(user.Language) {
+		user.Language = "en"
+		http.Redirect(w, r, fmt.Sprintf("/%s/%s", user.Language, user.ContentName), http.StatusMovedPermanently)
+		return
+	}
+
+	if !isValidContentName(user.ContentName) {
+		user.ContentName = "home"
+		http.Redirect(w, r, fmt.Sprintf("/%s/%s", user.Language, user.ContentName), http.StatusMovedPermanently)
+		return
+	}
+
 	var (
 		ctx  = gctx.New()
 		i18n = gi18n.New()
@@ -31,13 +105,14 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	user.Title = i18n.Translate(ctx, "title")
 	user.Name = i18n.Translate(ctx, "name")
 
-	t, err := template.ParseFiles("templates/index.tmpl", "templates/nav.tmpl")
+	// t, err := template.ParseFiles("templates/index.tmpl", "templates/nav.tmpl", "templates/header.tmpl", "templates/footer.tmpl")
 	// t, err := template.ParseFiles("templates/index.tmpl")
+	t, err := template.ParseGlob("templates/*.tmpl")
 	if err != nil {
 		fmt.Println("Template parsing error:", err)
 		return
 	}
-	if err := t.Execute(w, user); err != nil {
+	if err := t.ExecuteTemplate(w, "index.tmpl", user); err != nil {
 		log.Println("Template execution error: ", err)
 		return
 	}
@@ -60,6 +135,25 @@ func getLanguageFromHeader(header string) string {
 	}
 	return "en"
 }
+
+func isValidLang(lang string) bool {
+	for _, l := range languages_ranges {
+		if lang == l {
+			return true
+		}
+	}
+	return false
+}
+
+func isValidContentName(contentName string) bool {
+	for _, c := range containtlist {
+		if contentName == c {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
